4cd5d4cdda54902db46de787b9d0120ac52a86e1
FRC4909 <billericabionics@gmail.com>
2017-02-04 13:54:59 -0500

    Jason added loader subsystem and began teleop commacnds

--- src/org/usfirst/frc4909/STEAMWORKS/subsystems/Loader.java
+++ src/org/usfirst/frc4909/STEAMWORKS/subsystems/Loader.java
@@ -1,12 +1,25 @@
 package org.usfirst.frc4909.STEAMWORKS.subsystems;
 
+import org.usfirst.frc4909.STEAMWORKS.RobotMap;
+import org.usfirst.frc4909.STEAMWORKS.PID.PIDController;
+
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Loader extends Subsystem {
+	private final double lP = 0.005;
+	private final double lI = 0;
+	private final double lD = 0;
+	
+	private final AnalogPotentiometer loaderPivotPot = RobotMap.loaderPivotPot;
+    private final SpeedController loaderMotor = RobotMap.loaderMotor;
 
+    private final PIDController loaderPID = new PIDController(lP,lI,lD,0.3);
+    
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
