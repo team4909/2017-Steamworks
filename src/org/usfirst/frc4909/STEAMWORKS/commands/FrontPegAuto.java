52ff59bc3cb517732e2253a5b0e1aa84ca367eeb
Billerica Bionics <billericabionics@gmail.com>
2017-02-08 18:38:08 -0500

    Create Base Auto Peg

---
+++ src/org/usfirst/frc4909/STEAMWORKS/commands/FrontPegAuto.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc4909.STEAMWORKS.commands;
+
+import org.usfirst.frc4909.STEAMWORKS.Robot;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+
+/**
+ *
+ */
+public class FrontPegAuto extends InstantCommand {
+
+    public FrontPegAuto() {
+        super();
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called once when the command executes
+    protected void initialize() {
+    	
+    	Robot.drivetrain.driveStraightEncoder(96);
+    	
+    }
+
+}
