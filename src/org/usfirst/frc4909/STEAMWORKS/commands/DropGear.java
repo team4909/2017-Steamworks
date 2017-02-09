d7725f4aadff1f67f9d5b98cfcef6be248a100cb
Roshan Ravi <roshanravi10@gmail.com>
2017-02-09 01:03:14 -0500

    Add Construct.

--- src/org/usfirst/frc4909/STEAMWORKS/commands/DropGear.java
+++ src/org/usfirst/frc4909/STEAMWORKS/commands/DropGear.java
@@ -3,5 +3,9 @@ package org.usfirst.frc4909.STEAMWORKS.commands;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
 public class DropGear extends InstantCommand {
+    public DropGear() {
+        super();
+    }
+
     protected void initialize() {}
 }
