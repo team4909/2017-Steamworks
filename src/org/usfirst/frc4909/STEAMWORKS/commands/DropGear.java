e71c5b22b2c32d479b0845358aaeb828390c153d
Billerica Bionics <billericabionics@gmail.com>
2017-02-10 17:30:13 -0500

    Commit required for update from master

--- src/org/usfirst/frc4909/STEAMWORKS/commands/DropGear.java
+++ src/org/usfirst/frc4909/STEAMWORKS/commands/DropGear.java
@@ -1,17 +1,36 @@
 package org.usfirst.frc4909.STEAMWORKS.commands;
 
-import org.usfirst.frc4909.STEAMWORKS.Robot;
+import edu.wpi.first.wpilibj.command.Command;
 
-import edu.wpi.first.wpilibj.command.InstantCommand;
+/**
+ *
+ */
+public class DropGear extends Command {
 
-public class DropGear extends InstantCommand {
     public DropGear() {
-        super();
-        
-        requires(Robot.loader);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
     }
 
+    // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.loader.dropGear();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
     }
 }
