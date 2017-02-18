cff53f293ea118a6b756f4f2d2e68098e9a7b8b4
Billerica Bionics <billericabionics@gmail.com>
2017-02-18 13:25:52 -0500

    Fix Loader

--- src/org/usfirst/frc4909/STEAMWORKS/subsystems/Loader.java
+++ src/org/usfirst/frc4909/STEAMWORKS/subsystems/Loader.java
@@ -2,52 +2,26 @@ package org.usfirst.frc4909.STEAMWORKS.subsystems;
 
 import org.usfirst.frc4909.STEAMWORKS.Robot;
 import org.usfirst.frc4909.STEAMWORKS.RobotMap;
-import org.usfirst.frc4909.STEAMWORKS.PID.PIDController;
-import org.usfirst.frc4909.STEAMWORKS.utils.Subsystem;
+import org.usfirst.frc4909.STEAMWORKS.utils.PIDConstants;
+import org.usfirst.frc4909.STEAMWORKS.utils.PotentiometerPIDSubsystem;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.SpeedController; 
 
-public class Loader extends Subsystem {
-	private double targetTime;
-	
-	private final double lP = 0.08;
-	private final double lI = 0;
-	private final double lD = 0;
-		
-	private final AnalogPotentiometer loaderPivotPot = RobotMap.loaderPivotPot;
-    private final SpeedController loaderMotor = RobotMap.loaderMotor;
-
-    public final PIDController loaderPID = new PIDController(lP,lI,lD,0.3);
-
-	public double getAngle(){
-    	return loaderPivotPot.get();
+public class Loader extends PotentiometerPIDSubsystem {
+    public AnalogPotentiometer getPotentiometer(){
+    	return RobotMap.loaderPivotPot;
     }
-	
-	public void setSpeed(double speed){
-		loaderMotor.set(speed);
-	}
-	
-	public void initPID(){
-    	targetTime = Timer.getFPGATimestamp();
-    	
-    	loaderPID.resetPID();
-	}
-	
-	public void setPosition(int position){
-		loaderPID.atTarget = false;
-		
-		double targetAngle = Robot.config.loaderAngles[position];
-		double currentAngle = Robot.loader.getAngle();
-		
-		loaderMotor.set((loaderPID.calcPID(targetAngle, currentAngle, 2)));
-		
-		if(!loaderPID.isDone())
-			targetTime = Timer.getFPGATimestamp();
-	}
-	
-	public boolean isFinished(){
-		return Timer.getFPGATimestamp()-targetTime > .5;
-	}
+    
+    public SpeedController getMotorController(){
+    	return RobotMap.loaderMotor;
+    }
+    
+    public double[] getPositions(){
+    	return Robot.config.loaderAngles;
+    }
+    
+    public PIDConstants getPID(){
+    	return new PIDConstants(0.08, 0, 0, 0.3);
+    }
 }
