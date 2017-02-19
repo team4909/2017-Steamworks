147e4f82e85b9e242a193f2ad800d89afa49141f
Roshan Ravi <roshanravi10@gmail.com>
2017-02-19 11:45:11 -0500

    Work on Autos

--- src/org/usfirst/frc4909/STEAMWORKS/commands/auto/BreakBaseline.java
+++ src/org/usfirst/frc4909/STEAMWORKS/commands/auto/BreakBaseline.java
@@ -2,12 +2,14 @@ package org.usfirst.frc4909.STEAMWORKS.commands.auto;
 
 import org.usfirst.frc4909.STEAMWORKS.commands.drive.semiauto.DriveDistance;
 import org.usfirst.frc4909.STEAMWORKS.commands.drive.semiauto.ShiftToState;
+import org.usfirst.frc4909.STEAMWORKS.commands.loader.HoldGear;
 import org.usfirst.frc4909.STEAMWORKS.utils.devices.drivetrain.ShiftingRobotDrive.Gear;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class BreakBaseline extends CommandGroup {
     public BreakBaseline (){
+    	addSequential(new HoldGear());
     	addSequential(new ShiftToState(Gear.High));
     	addSequential(new DriveDistance(10));
     }
--- src/org/usfirst/frc4909/STEAMWORKS/commands/auto/PlaceFrontGearEncoder.java
+++ src/org/usfirst/frc4909/STEAMWORKS/commands/auto/PlaceFrontGearEncoder.java
@@ -1,7 +1,18 @@
 package org.usfirst.frc4909.STEAMWORKS.commands.auto;
 
+import org.usfirst.frc4909.STEAMWORKS.commands.drive.semiauto.*;
+import org.usfirst.frc4909.STEAMWORKS.commands.loader.*;
+import org.usfirst.frc4909.STEAMWORKS.utils.devices.drivetrain.ShiftingRobotDrive.Gear;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class PlaceFrontGearEncoder extends CommandGroup {
-    public PlaceFrontGearEncoder (){}
+    public PlaceFrontGearEncoder (){
+    	addSequential(new HoldGear());
+    	addSequential(new ShiftToState(Gear.High));
+    	addSequential(new DriveDistance(10));
+    	addSequential(new DropGear());
+    	addSequential(new DriveDistance(-5));
+    	addSequential(new HoldGear());
+    }
 }
